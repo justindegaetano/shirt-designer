26766c450f5a92cdd3f365c7d54c0861ddfd6f43
justindegaetano <105392908+justindegaetano@users.noreply.github.com>
2023-09-06 13:27:01 -0400

    Reverted to OpenAI API v3

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -1,14 +1,15 @@
 import express from "express";
 import * as dotenv from 'dotenv';
-import OpenAI from 'openai';
+import { Configuration, OpenAIApi } from "openai";
 
 dotenv.config();
 
 const router = express.Router();
 
-const openai = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
   });
+  const openai = new OpenAIApi(configuration);
 
 router.route('/').get((req, res) => {
     res.status(200).json({ message: "Hello from DALL.E ROUTES" })
