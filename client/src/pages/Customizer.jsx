c69610431fd87dd29404f82c1cec01129745957d
justindegaetano <105392908+justindegaetano@users.noreply.github.com>
2023-09-07 14:10:16 -0400

    Uploaded API host URLs

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -54,7 +54,7 @@ const Customizer = () => {
     if (!prompt) return alert('Please enter a prompt');
     try {
       setGeneratingImg(true);
-      const response = await fetch('http://localhost:8080/api/v1/dalle', {
+      const response = await fetch('http://localhost:8080/api/v1/dalle' || 'https://shirt-designer-api.onrender.com/api/v1/dalle', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
